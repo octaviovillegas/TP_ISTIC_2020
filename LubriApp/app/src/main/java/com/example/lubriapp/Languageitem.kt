24cb79e8df0044ff99a9cfc045fbd8628054c648
MATY <matyvernola@gmail.com>
2020-10-08 00:00:40 -0300

    Segundo commit, 1er Sprint con la documentación de la app, osea, que hace cada actividad y a quien le madna x dato

---
+++ LubriApp/app/src/main/java/com/example/lubriapp/Languageitem.kt
@@ -0,0 +1,15 @@
+package com.example.lubriapp
+
+class Languageitem {
+
+    var icons: Int ? = 0
+    var name: String ? = null
+
+    constructor(icons: Int?, name: String?) {
+        this.icons = icons
+        this.name = name
+
+    }
+
+
+}
