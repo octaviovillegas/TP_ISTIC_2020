96a93ca7f2985406649ffacf3275e67dbe418429
DionelShiftHA <dionel.marmori@shiftha.com>
2020-10-17 15:36:15 -0300

    primer commit3

---
+++ DInverCalculos/app/src/main/java/com/example/dinvercalculos/Registro.kt
@@ -0,0 +1,12 @@
+package com.example.dinvercalculos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
