2c74030b268be00117fd0dec3554158c0a2abead
P4B0N <pbonaduce@iiconcepcion.edu.ar>
2020-10-21 19:56:21 -0300

    implementando lector QR

---
+++ TP Final_V.2/app/src/main/java/com/p4b0n/listviewsqlite/ProductoActivity.kt
@@ -0,0 +1,84 @@
+package com.p4b0n.listviewsqlite
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_producto.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ProductoActivity : AppCompatActivity() {
+
+    private lateinit var database: AppDatabase
+    private lateinit var producto: Producto
+    private lateinit var productoLiveData: LiveData<Producto>
+    private val EDIT_ACTIVITY = 49
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_producto)
+
+        database = AppDatabase.getDatabase(this)
+
+        val idProducto = intent.getIntExtra("id", 0)
+
+        val imageUri = ImageController.getImageUri(this, idProducto.toLong())
+        imagen.setImageURI(imageUri)
+
+        productoLiveData = database.productos().get(idProducto)
+
+        productoLiveData.observe(this, Observer {
+            producto = it
+
+            nombre_producto.text = producto.nombre
+            precio_producto.text = "$${producto.precio}"
+            detalles_producto.text = producto.descripcion
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.producto_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.edit_item -> {
+                val intent = Intent(this, NuevoProductoActivity::class.java)
+                intent.putExtra("producto", producto)
+                startActivityForResult(intent, EDIT_ACTIVITY)
+            }
+
+            R.id.delete_item -> {
+                productoLiveData.removeObservers(this)
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    database.productos().delete(producto)
+                    ImageController.deleteImage(this@ProductoActivity, producto.idProducto.toLong())
+                    this@ProductoActivity.finish()
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when {
+            requestCode == EDIT_ACTIVITY && resultCode == Activity.RESULT_OK -> {
+                imagen.setImageURI(data!!.data)
+            }
+        }
+    }
+}
---
+++ TP Final_V.2/app/src/main/java/com/p4b0n/listviewsqlite/ProductosAdapter.kt
@@ -0,0 +1,28 @@
+package com.p4b0n.listviewsqlite
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.item_producto.view.*
+
+class ProductosAdapter(private val mContext: Context, private val listaProductos: List<Producto>) : ArrayAdapter<Producto>(mContext, 0, listaProductos) {
+    @SuppressLint("ViewHolder", "SetTextI18n")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val layout = LayoutInflater.from(mContext).inflate(R.layout.item_producto, parent, false)
+
+        val producto = listaProductos[position]
+
+        layout.nombre.text = producto.nombre
+        layout.precio.text = "$${producto.precio}"
+
+        val imageUri = ImageController.getImageUri(mContext, producto.idProducto.toLong())
+
+        layout.imageView.setImageURI(imageUri)
+
+        return layout
+    }
+
+}
