49a4b265a9a7ed8bdbdcb3dca78ab082322734bf
P4B0N <pbonaduce@iiconcepcion.edu.ar>
2020-10-28 17:15:12 -0300

    carga codigo de barras desde scanner

---
+++ TP Final_V.3/app/src/main/java/com/p4b0n/listviewsqlite/NuevoProductoActivity.kt
@@ -0,0 +1,109 @@
+package com.p4b0n.listviewsqlite
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_nuevo_producto.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NuevoProductoActivity : AppCompatActivity() {
+
+    private val SELECT_ACTIVITY = 50
+    private var imageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_producto)
+
+        findViewById<TextView>(R.id.codBar_et)
+
+        recogerExtras()
+
+
+        var idProducto: Int? = null
+
+        if (intent.hasExtra("producto")) {
+            val producto = intent.extras?.getSerializable("producto") as Producto
+
+            nombre_et.setText(producto.nombre)
+            precio_et.setText(producto.precio.toString())
+            descripcion_et.setText(producto.descripcion)
+            codBar_et.setText(producto.codigo)
+            idProducto = producto.idProducto
+
+            val imageUri = ImageController.getImageUri(this, idProducto.toLong())
+            imageSelect_iv.setImageURI(imageUri)
+        }
+
+        val database = AppDatabase.getDatabase(this)
+
+        save_btn.setOnClickListener {
+            val nombre = nombre_et.text.toString()
+            val precio = precio_et.text.toString().toDouble()
+            val descripcion = descripcion_et.text.toString()
+            val codigo = codBar_et.text.toString()
+
+            val producto = Producto(nombre, precio, descripcion, codigo, R.drawable.ic_launcher_background)
+
+            if (idProducto != null) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    producto.idProducto = idProducto
+
+                    database.productos().update(producto)
+
+                    imageUri?.let {
+                        val intent = Intent()
+                        intent.data = it
+                        setResult(Activity.RESULT_OK, intent)
+                        ImageController.saveImage(this@NuevoProductoActivity, idProducto.toLong(), it)
+                    }
+
+                    this@NuevoProductoActivity.finish()
+                }
+            } else {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val id = database.productos().insertAll(producto)[0]
+
+                    imageUri?.let {
+                        ImageController.saveImage(this@NuevoProductoActivity, id, it)
+                    }
+
+                    this@NuevoProductoActivity.finish()
+                }
+            }
+        }
+
+        btn_qr.setOnClickListener {
+            val unIntento = Intent(this, LectorQR::class.java)
+            startActivity(unIntento)
+        }
+
+        imageSelect_iv.setOnClickListener {
+            ImageController.selectPhotoFromGallery(this, SELECT_ACTIVITY)
+        }
+    }
+
+    private fun recogerExtras() {
+
+        val extras= intent.extras
+        val s = extras?.getString("texto")
+        codBar_et.setText(s)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when {
+            requestCode == SELECT_ACTIVITY && resultCode == Activity.RESULT_OK -> {
+                imageUri = data!!.data
+
+                imageSelect_iv.setImageURI(imageUri)
+            }
+        }
+    }
+}
---
+++ TP Final_V.3/app/src/main/java/com/p4b0n/listviewsqlite/P4B0N.kt
@@ -0,0 +1,62 @@
+package com.p4b0n.listviewsqlite
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_p4_b0_n.*
+
+class P4B0N : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p4_b0_n)
+
+
+                btnContinuar.setOnClickListener {
+                    val unIntento = Intent(this, LoginActivity::class.java)
+                    startActivity(unIntento)
+
+                }
+                IbtnFacebook.setOnClickListener{
+                    val uri = Uri.parse("http://www.facebook.com/")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
+
+                IbtnGit.setOnClickListener{
+                    val uri = Uri.parse("http://www.github.com/")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
+
+                IbtnGmail.setOnClickListener{
+                    val uri = Uri.parse("http://www.gmail.com/")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
+
+                IbtnInstagram.setOnClickListener{
+                    val uri = Uri.parse("http://www.instagram.com/")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
+
+                IbtnLinkedin.setOnClickListener{
+                    val uri = Uri.parse("http://www.linkedin.com/")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
+
+                IbtnTwitter.setOnClickListener{
+                    val uri = Uri.parse("http://www.twitter.com/")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
+
+                IbtnYoutube.setOnClickListener{
+                    val uri = Uri.parse("http://www.youtube.com/")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
+            }
+        }
