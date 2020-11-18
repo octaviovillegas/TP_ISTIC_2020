db11f9098ab13300f3dda9339ec44e0e52b5957a
DionelShiftHA <dionel.marmori@shiftha.com>
2020-11-18 19:53:27 -0300

    Activi3.

--- DInverCalculos/app/src/main/java/com/example/dinvercalculos/MercadoInteligente.kt
+++ DInverCalculos/app/src/main/java/com/example/dinvercalculos/MercadoInteligente.kt
@@ -4,23 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.app.AlertDialog
+import android.widget.ListView
 import android.widget.Toast
 import java.lang.reflect.MalformedParametersException
 
 class MercadoInteligente : AppCompatActivity() {
 
-    lateinit var mAlertButton: Button
-
-    val  programming_lang = arrayOf("Creciendo - bajo","Descendente - bajo","Creciendo - Alto","Descendente - alto")
-
-    var checkedItem = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mercado_inteligente)
 
-        mAlertButton = findViewById(R.id.btnVolumen)
+        val btnVolumen = findViewById<Button>(R.id.btnVolumen)
+        val btnEMA = findViewById<Button>(R.id.btnEMA)
+        val btnRSI = findViewById<Button>(R.id.btnRSI)
 
+        btnVolumen.setOnClickListener {
+            volumen()
+        }
+        btnEMA.setOnClickListener {
+            ema()
+        }
+        btnRSI.setOnClickListener {
+            rsi()
+        }
+
+    }
+
+    //Funcion para el boton volumen
+    fun volumen(){
+        lateinit var mAlertButton: Button
+        val  programming_lang = arrayOf("Creciendo - bajo","Descendente - bajo","Creciendo - Alto","Descendente - alto")
+        var checkedItem = 1
+
+        mAlertButton = findViewById(R.id.btnVolumen)
         mAlertButton.setOnClickListener {
 
             val mAlertDialogBuilder = AlertDialog.Builder(this@MercadoInteligente)
@@ -38,7 +55,7 @@ class MercadoInteligente : AppCompatActivity() {
             }
             mAlertDialogBuilder.setPositiveButton("Aceptar"){_, _->
                 Toast.makeText(this@MercadoInteligente, "Volumen seleccionado", Toast.LENGTH_LONG).show()
-        }
+            }
             mAlertDialogBuilder.setNegativeButton("Borrar"){_, _ ->
                 Toast.makeText(this@MercadoInteligente, "Seleccion borrada", Toast.LENGTH_LONG).show()
             }
@@ -48,7 +65,79 @@ class MercadoInteligente : AppCompatActivity() {
             val mAlertDialog = mAlertDialogBuilder.create()
             mAlertDialog.show()
         }
+    }//Finaliza funcion volumen
 
-    }
+    //Funcion para el boton EMA
+    fun ema(){
+        lateinit var mAlertButton: Button
+        val  programming_lang = arrayOf("Creciendo - bajo","Descendente - bajo","Creciendo - Alto","Descendente - alto")
+        var checkedItem = 1
+
+        mAlertButton = findViewById(R.id.btnEMA)
+        mAlertButton.setOnClickListener {
+
+            val mAlertDialogBuilder = AlertDialog.Builder(this@MercadoInteligente)
+
+            mAlertDialogBuilder.setTitle("EMA Exponencial")
+            mAlertDialogBuilder.setIcon(R.mipmap.ic_launcher)
+            mAlertDialogBuilder.setCancelable(false)
+
+            mAlertDialogBuilder.setSingleChoiceItems(programming_lang, checkedItem) {dialog, which ->
+                when (which){
+                    which -> {
+                        Toast.makeText(this@MercadoInteligente, programming_lang[which], Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            mAlertDialogBuilder.setPositiveButton("Aceptar"){_, _->
+                Toast.makeText(this@MercadoInteligente, "Volumen seleccionado", Toast.LENGTH_LONG).show()
+            }
+            mAlertDialogBuilder.setNegativeButton("Borrar"){_, _ ->
+                Toast.makeText(this@MercadoInteligente, "Seleccion borrada", Toast.LENGTH_LONG).show()
+            }
+            mAlertDialogBuilder.setNeutralButton("Cancelar"){_, _ ->
+                Toast.makeText(this@MercadoInteligente, "Accion cancelada", Toast.LENGTH_LONG).show()
+            }
+            val mAlertDialog = mAlertDialogBuilder.create()
+            mAlertDialog.show()
+        }
+    }//Finaliza funcion EMA
+
+    //Funcion para el boton RSI
+    fun rsi(){
+        lateinit var mAlertButton: Button
+        val  programming_lang = arrayOf("Creciendo - bajo","Descendente - bajo","Creciendo - Alto","Descendente - alto")
+        var checkedItem = 1
+
+        mAlertButton = findViewById(R.id.btnRSI)
+        mAlertButton.setOnClickListener {
+
+            val mAlertDialogBuilder = AlertDialog.Builder(this@MercadoInteligente)
+
+            mAlertDialogBuilder.setTitle("EMA Exponencial")
+            mAlertDialogBuilder.setIcon(R.mipmap.ic_launcher)
+            mAlertDialogBuilder.setCancelable(false)
+
+            mAlertDialogBuilder.setSingleChoiceItems(programming_lang, checkedItem) {dialog, which ->
+                when (which){
+                    which -> {
+                        Toast.makeText(this@MercadoInteligente, programming_lang[which], Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            mAlertDialogBuilder.setPositiveButton("Aceptar"){_, _->
+                Toast.makeText(this@MercadoInteligente, "Volumen seleccionado", Toast.LENGTH_LONG).show()
+            }
+            mAlertDialogBuilder.setNegativeButton("Borrar"){_, _ ->
+                Toast.makeText(this@MercadoInteligente, "Seleccion borrada", Toast.LENGTH_LONG).show()
+            }
+            mAlertDialogBuilder.setNeutralButton("Cancelar"){_, _ ->
+                Toast.makeText(this@MercadoInteligente, "Accion cancelada", Toast.LENGTH_LONG).show()
+            }
+            val mAlertDialog = mAlertDialogBuilder.create()
+            mAlertDialog.show()
+        }
+    }//Finaliza funcion RSI
+
 
 }
